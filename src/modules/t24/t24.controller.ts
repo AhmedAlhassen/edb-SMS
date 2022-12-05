90be2f145799f585d925f52d21db48015f61c6c2
AhmedAlhassen <makaveli-tag_alsir@yahoo.com>
2022-12-05 13:13:21 +0000

    controller sms log

--- src/modules/t24/t24.controller.ts
+++ src/modules/t24/t24.controller.ts
@@ -1,24 +1,33 @@
-import { Body, Controller, Inject, Logger, LoggerService, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Logger,
+  LoggerService,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { T24Service } from './t24.service';
 
 @Controller('t24')
 export class T24Controller {
-    
-    constructor(private t24Service: T24Service,@Inject(Logger) private readonly logger: LoggerService){}
-    
-    @Post('sendsms')
-    async sendSms(@Req() request: Request){
-        // this.logger.log('T24Controller',{message})
-    //   this.logger.log('T24Controller',request.body)
-  
-        const smsobj = await this.t24Service.parseSms(request?.body);
-        
-        // this.logger.log('T24Controller', {smsobj})
-        const data = await this.t24Service.sendSmS(smsobj);
-        
-        this.logger.log('T24Controller',data)
-        
-        return "<?xml version=\"1.0\"?><clickAPI><sendMsgResp>003, Success<sequence_no></sequence_no></sendMsgResp></clickAPI>"
-        
-    }
+  constructor(
+    private t24Service: T24Service,
+    @Inject(Logger) private readonly logger: LoggerService,
+  ) {}
+
+  @Post('sendsms')
+  async sendSms(@Req() request: Request) {
+    // this.logger.log('T24Controller',{message})
+    this.logger.log('T24Controller', request.body);
+
+    const smsobj = await this.t24Service.parseSms(request?.body);
+
+    // this.logger.log('T24Controller', {smsobj})
+    const data = await this.t24Service.sendSmS(smsobj);
+
+    this.logger.log('T24Controller', data);
+
+    return '<?xml version="1.0"?><clickAPI><sendMsgResp>003, Success<sequence_no></sequence_no></sendMsgResp></clickAPI>';
+  }
 }
